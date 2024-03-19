85ec1dee0aa91887af198e0277434688b8797fd3
amirholya01 <amirholya01@gmail.com>
2024-03-19 19:13:37 +0100

    primary-category-class

---
+++ app/controller/admin/CategoryController.js
@@ -0,0 +1,9 @@
+// Import necessary modules and dependencies
+const Controller = require("../Controller");  // Import the base controller
+
+class CategoryController extends Controller{}
+
+
+module.exports = {
+    CategoryController : new CategoryController()
+}
