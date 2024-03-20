1e6273be6f2152acc4f3dca0c37195b4149fa8ff
amirholya01 <amirholya01@gmail.com>
2024-03-20 16:05:48 +0100

    list of courses

--- app/controller/admin/CourseController.js
+++ app/controller/admin/CourseController.js
@@ -1,8 +1,22 @@
 const Controller = require("../Controller");
+const {CourseModel} = require("../../model/course");
+const {StatusCodes: HttpStatus} = require("http-status-codes");
 
 
 class CourseController extends Controller{
-
+    async getAllCourses(req, res, next){
+        try {
+            const courses = await CourseModel.find({}).sort({_id:-1});
+            return res.status(HttpStatus.OK).json({
+                statusCode: HttpStatus.OK,
+                data: {
+                    courses
+                }
+            })
+        }catch (e) {
+            next(e);
+        }
+    }
 }
 
 module.exports = {
--- app/router/admin/adminRoutes.js
+++ app/router/admin/adminRoutes.js
@@ -1,4 +1,5 @@
 const { AdminApiCategoryRouter} = require("./category");
+const {AdminApiCourseRouter} = require("./course");
 
 const router = require("express").Router();
 
@@ -7,9 +8,12 @@ const router = require("express").Router();
  *  tags:
  *     -    name: Admin-Panel
  *          description: all methods and routes for course
+ *     -    name: Course(Admin-Panel)
+ *          description: all methods and routes for course
  *     -    name: Category(Admin-Panel)
  *          description: all methods and routes for category
  */
+router.use("/course", AdminApiCourseRouter);
 router.use("/category", AdminApiCategoryRouter);
 
 
