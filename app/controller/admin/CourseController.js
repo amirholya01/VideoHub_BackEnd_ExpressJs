62e66eabd788fe08a75dca8e2120b35a70ae6fa8
amirholya01 <amirholya01@gmail.com>
2024-03-26 19:12:18 +0100

    searching for courses

--- app/controller/admin/CourseController.js
+++ app/controller/admin/CourseController.js
@@ -6,7 +6,11 @@ const {StatusCodes: HttpStatus} = require("http-status-codes");
 class CourseController extends Controller{
     async getAllCourses(req, res, next){
         try {
-            const courses = await CourseModel.find({}).sort({_id:-1});
+            const {search} = req.query;
+            let courses;
+
+            if (search) courses = await CourseModel.find({$text : {$search : search}}).sort({_id:-1});
+            else courses = await CourseModel.find({}).sort({_id:-1});
             return res.status(HttpStatus.OK).json({
                 statusCode: HttpStatus.OK,
                 data: {
